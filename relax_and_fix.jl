e343c35e668ca170b0597f7b9762e47e44d030b8
VirginiaVerna <verna.virginia99@gmail.com>
2026-03-03 16:19:41 +0100

    Updated

--- relax_and_fix.jl
+++ relax_and_fix.jl
@@ -9,9 +9,9 @@ function relax_and_fix(model, time_blocks, binary_blocks)
     m = copy(model)
     set_optimizer(m, Gurobi.Optimizer)
     set_attribute(m, "OutputFlag", 0)
-    #set_attribute(m, "TimeLimit", 150.0)
+    set_attribute(m, "TimeLimit", 50.0)
     #set_attribute(m, "TimeLimit", 1500.0) # 25 minutes per block
-    set_attribute(m, "TimeLimit", 300.0) # 5 minutes per block
+    #set_attribute(m, "TimeLimit", 300.0) # 5 minutes per block
     
 
 
@@ -24,8 +24,8 @@ function relax_and_fix(model, time_blocks, binary_blocks)
 
     all_binaries = [v for v in all_vars if is_binary(v)]
 
-    #binaries_current=binary_var_block_ls(m, time_blocks) # LSP version
-    binaries_current=binary_var_block_tm(m, time_blocks) # TM version
+    binaries_current=binary_var_block_ls(m, time_blocks) # LSP version
+    #binaries_current=binary_var_block_tm(m, time_blocks) # TM version
     
     for (i, block) in enumerate(time_blocks)
         
@@ -56,10 +56,20 @@ function relax_and_fix(model, time_blocks, binary_blocks)
     
         # Solve the subproblem
         optimize!(m)
+        
+        for v in binary_blocks[i]
+            val = value(v)
+            if val > 0.5
+                println("  Selected: $v = $val")
+            end
+        end
+    
+
+
 
         #----------- CHECK -----------------------
         # 3. DEBUGGING: Print state if Infeasible or if you want to track Block 4/5
-
+        #=
         status = termination_status(m)
         if status != MOI.OPTIMAL && status != MOI.TIME_LIMIT
             println("!!! INFEASIBILITY DETECTED at Block $i !!!")
@@ -72,12 +82,14 @@ function relax_and_fix(model, time_blocks, binary_blocks)
             end
             return nothing
         end
+        
 
         if !has_values(m)
             println("No values found for Block $i")
             return nothing
         end
 
+        
         # 4. Print current progress for x, y, z (where value > 0.5)
         println("Decisions made in Block $i:")
         for v in binaries_current[i]
@@ -87,21 +99,21 @@ function relax_and_fix(model, time_blocks, binary_blocks)
                 println("  Selected: $(v) = $(round(val, digits=2))")
             end
         end
-
+        =#
 
         #-----------------------------------
 
 
-        #=
+        
         status = termination_status(m)
         if status != MOI.OPTIMAL && status != MOI.TIME_LIMIT
-            println("Infeasible al blocco $i. Termino l'euristico.")
+            println("Infeasible at block $i. Stopped heuristic.")
             return nothing
         elseif !has_values(m)
-            println("Nessuna soluzione trovata nel blocco $i entro il tempo limite.")
+            println("No solution found in block $i within time limit.")
             return nothing
         end
-        =#
+        
     
         # save the values before fixing
         vals = Dict{VariableRef, Float64}()
@@ -125,12 +137,12 @@ function relax_and_fix(model, time_blocks, binary_blocks)
 
     status = termination_status(m)
     if status != MOI.OPTIMAL
-        println("Problema finale infeasible.")
+        println("Final problem infeasible.")
         return nothing
     end
 
     if !has_values(m)
-        println("Problema finale senza soluzione ammissibile (Stato: $status).")
+        println("Final problem has no feasible solution (Status: $status).")
         return nothing
     end
 
