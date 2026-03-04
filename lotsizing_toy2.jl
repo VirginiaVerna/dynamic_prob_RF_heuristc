798d7ca4b0792406e712803565eecf5e85879e3e
VirginiaVerna <verna.virginia99@gmail.com>
2026-03-04 10:10:58 +0100

    Updated

--- lotsizing_toy2.jl
+++ lotsizing_toy2.jl
@@ -7,14 +7,22 @@ Random.seed!(125)
 
 products=["sugar", "salt", "pepper", "oil"]
 periods=[1,2,3,4,5,6,7,8,9,10,11,12]
-machines=["machine1", "machine2", "machine3"]
+machines=["machine1","machine2"]
 
 # sparse demand
-d=[rand() < 0.4 ? rand(5.0:1.0:10.0) : 0.0 for j in 1:length(products), t in 1:length(periods)]
-c=[rand(1:1:4) for j in 1:length(products), t in 1:length(periods)]
-g=[rand(100:100:300) for j in 1:length(products), t in 1:length(periods)]
-f=[rand(3:1:5) for j in 1:length(products), t in 1:length(periods)]
-s0=[rand(1.0:1.0:3.0) for j in 1:length(products)]
+d=[rand() < 0.4 ? rand(18.0:2.0:30.0) : 0.0 for j in 1:length(products), t in 1:length(periods)]
+
+# unit production cost
+c=[rand(10:2:20) for j in 1:length(products), t in 1:length(periods)]
+
+# setup cost
+g=[rand(100:50:300) for j in 1:length(products), t in 1:length(periods)]
+
+# inventory cost
+f=[rand(0.3:0.2:1.5) for j in 1:length(products), t in 1:length(periods)]
+
+# initial inventory
+s0=[rand(0.0:1.0:2.0) for j in 1:length(products)]
 
 c = DenseAxisArray(c, products, periods)
 g = DenseAxisArray(g, products, periods)
@@ -23,6 +31,6 @@ d = DenseAxisArray(d, products, periods)
 s0 = DenseAxisArray(s0, products)
 
 
-R=20
-K=11
-Q=20
+R=27
+K=20
+Q=25
